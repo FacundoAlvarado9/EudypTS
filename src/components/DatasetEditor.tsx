cad72e27d6538e091fbde2e1899c62fc2de5cca4
Facundo Alvarado <facundoalvarado9@gmail.com>
2025-08-04 13:44:53 -0300

    Added new logic for loading datasets onto the datatable

--- src/components/DatasetEditor.tsx
+++ src/components/DatasetEditor.tsx
@@ -3,15 +3,23 @@ import parseCSVFile from "../utils/csvParser";
 import FileUploader from "./FileUploader";
 import DataTable from "./DataTable";
 import type { TableData } from "../types/Dataset";
+import DateColumnSelector from "./DateColumnSelector";
 
 type DatasetEditorProps = {
+    tableName : string; 
+    table : (TableData | null);
     onDatasetChange : (dataset : TableData) => void;
     onError : (error : any) => void;
+    onChangeDateColumn : (columnId : number) => void;
 }
 
-export default function DatasetEditor({ onDatasetChange, onError } : DatasetEditorProps){
+export default function DatasetEditor({ tableName, table, onDatasetChange, onError, onChangeDateColumn } : DatasetEditorProps){
 
-    const [parsedDataset, setParsedDataset] = useState<TableData | null>(null);
+    const [parsedFileCount, setParsedLoadCount] = useState<number>(0);
+
+    const increaseParsedFileCount = () => {
+        setParsedLoadCount(parsedFileCount+1);
+    }
 
     const handleFileChange = async (file : File | null) => {
         if (file){
@@ -19,22 +27,20 @@ export default function DatasetEditor({ onDatasetChange, onError } : DatasetEdit
                 const parsedData = await parseCSVFile(file) as { data: object[] };
                 const headers = Object.keys(parsedData.data[0]);
                 const data = parsedData.data.map(obj => Object.values(obj));
-                setParsedDataset({headers: headers, data: data} as TableData);
+                increaseParsedFileCount();
+                onDatasetChange({headers: headers, data: data} as TableData);
             } catch (error) {
-                onError(error);
-            }            
+                onError("Parsing error -  " + error);
+            }
         }
     }
 
-    const onDataChange = (dataset : TableData) => {        
-        onDatasetChange(dataset as TableData);
-    }
-
     return (        
-        <>
+        <>        
         <FileUploader onFileChange={handleFileChange}/>
-        {parsedDataset && (<>
-            <DataTable dataset={parsedDataset} onChange={onDataChange} />
+        {table && (<>
+            <DateColumnSelector id={tableName} table={table} onChange={onChangeDateColumn} />
+            <DataTable dataset={table} onChange={onDatasetChange} dataLoadCount={parsedFileCount}/>
         </>)}
         </>
     )
